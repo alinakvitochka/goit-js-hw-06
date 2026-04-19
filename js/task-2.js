f78962fc82bea274107366073f9a4bbb9b7c74f4
Alina <alinapiatakova1309@gmail.com>
2026-04-19 21:20:33 +0300

    fix

--- js/task-2.js
+++ js/task-2.js
@@ -1,24 +1,18 @@
 class Storage {
-  constructor(initialItems){
-this._items = initialItems;
+  #items
+  constructor(initialItems) {
+    this.items = initialItems;
   }
-getItems() {
-  return this._items;
+  getItems() {
+    return this.items;
+  }
+  addItem(newItem) {
+    this.items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    this.items = this.items.filter((item) => item !== itemToRemove);
+  }
 }
-addItem(newItem){
-  this._items.push(newItem)
-}
-removeItem(itemToRemove){
-  this._items= this._items.filter(item => item !== itemToRemove);
-}
-}
-
-
-
-
-
-
-
 
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
